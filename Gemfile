cbf8d6d1a3e072126a6a21367d2a54481376dfe8
Plamen Kolev <p.kolev22@gmail.com>
2017-06-26 12:24:33 +0300

    added basic fucntionality to send online articles to kindle, starting to do testing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source "https://rubygems.org"
 
+gem 'mailfactory', '~> 1.4'
 gem 'commander', '~> 4.4', '>= 4.4.3'
 gem 'dotenv', '~> 2.2', '>= 2.2.1'
+
+group :test do
+  gem 'rspec', '~> 3.6'
+end
---
+++ test/sending_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe "checkout", :type => :request do
+  ENV["ENVIRONMENT"] = 'test'
+  # show checkout with norwegian url
+  it "raises on empty configuration" do
+
+  end
+
+  it "raises on missing arguments in configuration" do
+  end
+
+  it "raises on malformed smtp server" do
+  end
+
+  it "raises on invalid domain" do
+  end
+
+  it "raises on invalid smtp username and recipient" do
+  end
+
+  it 'tries to contact mail server but is misconfigured(config is ok but fake username)' do
+  end
+
+end
